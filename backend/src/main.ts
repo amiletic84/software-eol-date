99160b1c8506a6465c261fbf7e17db8dcf2010a9
Aleksandar Miletic <aleksandar.miletic@endava.com>
2026-05-19 13:54:45 +0200

    added github actions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', "*"], credentials: true });
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3001);
 }
