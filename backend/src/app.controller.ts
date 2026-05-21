0d6c8df467c078c3b95d99b535dce4fcfb5ca3a9
Aleksandar Miletic <aleksandar.miletic@endava.com>
2026-05-21 17:40:52 +0200

    change on trigger deployment

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("health")
+  getHealth(): void {
+    // just return OK
+    return;
+  }
 }
